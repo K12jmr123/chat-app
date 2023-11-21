4b2e1f0976cfb8af8e4928e46d4a158d66d3b713
Theiuzel <gengieban.amado@gmail.com>
2023-11-21 11:32:31 +0800

    update

--- server/server.js
+++ server/server.js
@@ -3,11 +3,47 @@ const http = require("http");
 const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
+const mysql = require("mysql");
+// server/server.js
+const registerRouter = require('./register/register');
+const loginRouter = require('./login/login');
+const port = 4000;
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'chat-app'
+});
+
 
 const io = socketio(server);
 
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/client/index.html");
+    connection.connect((err, connection)=>{
+        if(err) {
+            console.error('Not Connected'+ err.stack);
+            return;
+        }
+    
+        console.log('Connected'+connnection.threadId);
+    
+        connection.end((err)=>{
+            if (err){
+                console.error(''+err.stack);
+                return;
+            }
+            console.log('Connection Closed');
+        });
+    });
 })
 
 io.on("connection", (socket) =>{
@@ -34,3 +70,6 @@ server.listen(4000, () =>{
     console.log("Server listening on PORT 4000");
 });
 
+
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
